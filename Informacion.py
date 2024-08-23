29e1cf415a2e1198659744d6b6fb1cb9e839f1c3
SimonBetancur <betancurbarrerasimon.personal@gmail.com>
2024-08-23 11:09:03 -0500

    Definitivamente no entiendo nada. Pero agregué informacion por si algo

---
+++ Informacion.py
@@ -0,0 +1,6 @@
+# las variables solo existen cuando se crea el objeto
+# la clase es la platilla, objeto es el elemento en memoria que guarda la informacion que uno quiere almacenar
+# metodo main funcion común que tiene como objetivo tener el bloque de codigo que va a manipular la informacion que se ingresará
+# en el flujo normal de codigo
+
+# Fork traer un repositorio a mi propia cuenta, generando una copia
